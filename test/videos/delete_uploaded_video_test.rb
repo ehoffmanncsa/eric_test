d7f905e3017ba079d984484fa2012ca7d9bb5454
Tiffany Rea <tiffy.nets@gmail.com>
2017-11-02 10:14:16 -0500

    created video modules for all the common methods to interact with videos and applied to delete an uploaded video test

--- test/videos/delete_uploaded_video_test.rb
+++ test/videos/delete_uploaded_video_test.rb
@@ -24,24 +24,12 @@ class DeleteUploadedVideoTest < Minitest::Test
   def test_delete_a_video
     POSSetup.new.buy_package(@recruit_email, @username, 'elite')
 
+    action = Video.new(@username, @recruit_email)
+    action.upload_video
+    action.teardown
+
     @ui.user_login(@username)
     @browser.get 'https://qa.ncsasports.org/clientrms/profile/video'
-
-    @browser.find_element(:class, 'js-upload-options').find_element(:class, 'upload-options-text').click
-    assert @browser.find_element(:id, 'profile-video-upload').displayed?, 'Cannot find Video Upload Session'
-
-    session = @browser.find_element(:class, 'action-buttons')
-    assert session.find_element(:class, 'button--cancel').enabled?, 'Upload Session Cancel button not found'
-    assert session.find_element(:class, 'button--primary').enabled?, 'Upload Session Upload button not found'
-
-    @browser.find_element(:id, 'uploaded_video_as_is').find_elements(:tag_name, 'option')[1].click
-    @browser.find_element(:id, 'uploaded_video_position').send_keys SecureRandom.hex(4)
-    @browser.find_element(:id, 'uploaded_video_jersey_number').send_keys SecureRandom.hex(4)
-    @browser.find_element(:id, 'uploaded_video_jersey_color').send_keys SecureRandom.hex(4)
-
-    path = File.absolute_path('test/videos/sample.mp4')
-    @browser.find_element(:id, 'profile-video-upload-file-input').send_keys path
-    @browser.find_element(:class, 'action-buttons').find_element(:class, 'button--primary').click; sleep 1
 
     @browser.find_element(:class, 'fa-remove').click
     modal = @browser.find_element(:class, 'video-confirm-modal')
