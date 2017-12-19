81d1d1ba974748de618f408588503dbe4b831f54
Tiffany Rea <tiffy.nets@gmail.com>
2017-12-19 09:39:04 -0600

    TS-194 Coach Regression - Log in - automation script created

---
+++ test/coach_rms/login_test.rb
@@ -0,0 +1,29 @@
+# encoding: utf-8
+require_relative '../test_helper'
+
+# TS-194: Coach Regression
+# UI Test: Coach RMS Login Test
+class CoachRMSLoginTest < Minitest::Test
+  def setup    
+    @ui = UI.new 'local', 'firefox'
+    @browser = @ui.driver
+    UIActions.setup(@browser)
+  end
+
+  def teardown
+    @browser.quit
+  end
+
+  def test_coach_rms_login
+  	UIActions.coach_rms_login; sleep 4
+
+  	expected_title = "Home | NCSA Coach Recruiting Management System"
+  	title = @browser.title
+  	assert_equal expected_title, title, "Page title: #{title} - Not as expected: #{expected_title}"
+
+  	expected_name = 'LaJay Ball'
+  	user_info = @browser.find_element(:class, 'header__user-info')
+  	user_name = user_info.find_element(:class, 'header__user-info__menu-button__user__data').text
+  	assert_equal expected_name, user_name, "User's name: #{user_name} - Not as expected #{expected_name}"
+  end
+end
