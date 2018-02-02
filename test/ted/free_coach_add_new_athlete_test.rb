57ff132742c4e8c09b2810dca167029dd3a31cef
Tiffany Rea <tiffy.nets@gmail.com>
2018-02-02 09:13:19 -0600

    TS-260 TED Regression - Add/Invite New Athlete as Free Coach - automation script created

---
+++ test/ted/free_coach_add_new_athlete_test.rb
@@ -0,0 +1,131 @@
+# encoding: utf-8
+require_relative '../test_helper'
+
+# UI TED Regression
+# TS-260: Add/Invite New Athlete as Free Coach
+class FreeCoachAddNewAthleteTest < Minitest::Test
+  def setup
+    @ui = UI.new 'local', 'firefox'
+    @browser = @ui.driver
+    UIActions.setup(@browser)
+    POSSetup.setup(@browser)
+
+    @gmail = GmailCalls.new
+    @gmail.get_connection
+    @gmail.mail_box = 'TED_Welcome'
+    @gmail.sender = 'TeamEdition@ncsasports.org'
+
+    @email = MakeRandom.email
+    @first_name = MakeRandom.name
+    @last_name = MakeRandom.name
+    @athlete_name = "#{@first_name} #{@last_name}"
+  end
+
+  def teardown
+    @browser.close
+  end
+
+  def go_to_athlete_tab
+    # go to administration -> athlete
+    Watir::Wait.until { @browser.element(:class, 'sidebar').visible? }
+    @browser.link(:text, 'Administration').click
+    Watir::Wait.until { @browser.element(:id, 'react-tabs-1').present? }
+    @browser.element(:id, 'react-tabs-2').click
+    Watir::Wait.until { @browser.element(:id, 'react-tabs-3').visible? }
+  end
+
+  def add_athlete
+    UIActions.ted_free_coach_login
+    go_to_athlete_tab
+
+    # find add athlete button and click
+    @browser.button(:text, 'Add Athlete').click
+
+    # fill out athlete form
+    Watir::Wait.until { @browser.element(:class, 'modal-content').visible? }
+    modal = @browser.element(:class, 'modal-content')
+    modal.elements(:tag_name, 'input')[0].send_keys @first_name              # first name
+    modal.elements(:tag_name, 'input')[1].send_keys @last_name               # last name
+    modal.elements(:tag_name, 'input')[2].send_keys MakeRandom.grad_yr       # graduation year
+    modal.elements(:tag_name, 'input')[3].send_keys MakeRandom.number(5)     # zipcode
+    modal.elements(:tag_name, 'input')[4].send_keys @email                   # email
+    modal.elements(:tag_name, 'input')[5].send_keys MakeRandom.number(10)    # phone
+    modal.button(:text, 'Add Athlete').click; sleep 1
+
+     # make sure athlete name shows up after added
+    assert (@browser.html.include? @athlete_name), 'Cannot find newly added Athlete'
+  end
+
+  def send_invite_email
+    # find and click the not sent button for the newly added athlete
+    # make sure Edit Athlete modal shows up before proceeding
+    table = @browser.table(:class, 'table--administration')
+    row = table.elements(:tag_name, 'tr').last
+    row.elements(:tag_name, 'td')[4].element(:class, 'btn-primary').click
+    assert @browser.element(:class, 'modal-content').visible?
+
+    modal = @browser.element(:class, 'modal-content')
+    modal.button(:text, 'Save & Invite').click; sleep 5
+
+    # make sure athlete status is now pending after email sent
+    status = row.elements(:tag_name, 'td')[4].text
+    assert_equal status, 'Pending', "Expected status #{status} to be Pending"
+
+    UIActions.clear_cookies
+  end
+
+  def check_email
+    emails = @gmail.get_emails_by_subject
+    refute_empty emails, 'No welcome email found after inviting athlete'
+    @gmail.delete(emails)
+  end
+
+  def check_athlete_profile
+    POSSetup.set_password(@email)
+    @browser.element(:class, 'fa-angle-down').click
+    navbar = @browser.element(:id, 'secondary-nav-menu')
+    refute (navbar.html.include? 'Membership Info'), 'Found membership option in menu'
+  end
+
+  def get_row_by_name
+    table = @browser.table(:class, 'table--administration')
+    rows = table.elements(:tag_name, 'tr').to_a; rows.shift
+    rows.detect { |r| r.elements(:tag_name, 'td')[0].text.eql? @athlete_name }
+  end
+
+  def check_athlete_accepted_status
+    UIActions.ted_free_coach_login
+    go_to_athlete_tab; sleep 3
+    row = get_row_by_name
+    status = row.elements(:tag_name, 'td')[4].text
+    assert_equal 'Accepted', status, "Expected status #{status} to be Accepted"
+  end
+
+  def delete_athlete
+    row = get_row_by_name
+    cog = row.elements(:tag_name, 'td').last.element(:class, 'fa-cog')
+    cog.click; sleep 1
+    modal = @browser.div(:class, 'modal-content')
+    modal.button(:text, 'Delete').click
+    small_modal = modal.div(:class, 'modal-content')
+    small_modal.button(:text, 'Delete').click; sleep 1
+
+    refute (@browser.html.include? @athlete_name), "Found deleted athlete #{@athlete_name}"
+  end
+
+  def check_team_directory
+    @browser.goto 'https://team-staging.ncsasports.org/team_directory'
+    msg = "Found deleted athlete #{@athlete_name} in team directory"
+    refute (@browser.html.include? @athlete_name), msg
+  end
+
+  def test_add_delete_new_athlete_as_free_coach
+    add_athlete
+    send_invite_email
+    check_email
+    check_athlete_profile
+    check_athlete_accepted_status
+    delete_athlete
+    check_team_directory
+  end
+end
