35edb7dc73ed1b52fb7a5563b7ab200b9db51d2f
Tiffany Rea <tiffy.nets@gmail.com>
2018-04-03 10:46:22 -0500

    TS-382 TED - Find College Filters - part 2 filter by selecting enrollment size and/or tuition rage from slide bars

---
+++ test/ted/find_colleges_by_slidebar_test.rb
@@ -0,0 +1,111 @@
+# encoding: utf-8
+require_relative '../test_helper'
+
+# TS-382: TED Regression
+# UI Test: Find College Filters
+
+=begin
+  Coach Admin Tiffany
+  Check college filters:
+    - Filter results by slide bar options: enrollment size, tuition
+    - Filter by each option individually
+    - Filter by combining both options
+=end
+
+class FindCollegesBySlideBarTest < Minitest::Test
+  def setup
+    @ui = UI.new 'local', 'firefox'
+    @browser = @ui.driver
+    UIActions.setup(@browser)
+    TED.setup(@browser)
+
+    goto_find_colleges
+  end
+
+  def teardown
+    @browser.close
+  end
+
+  def goto_find_colleges
+    UIActions.ted_login
+    TED.goto_colleges
+  end
+
+  def open_filter
+    @browser.button(:text, 'Filter Results').click
+  end
+
+  def apply_filters
+    @browser.button(:text, 'Apply Filters').click
+    UIActions.wait_for_spinner
+  end
+
+  def colleges
+    @browser.elements(:class, 'card-college').to_a
+  end
+
+  def select_filter(element_id)
+    # there are 6 selectors on slidebar
+    # where aria-value indicates:
+    # 0 for <1K, 1 for >= 5K, 2 for >= 10K, etc...
+    # select 10k filter in this case
+    section = @browser.div(:id, element_id)
+    slide_bar = section.element(:class, 'rc-slider-with-marks')
+    left_handle = slide_bar.element(:class, 'rc-slider-handle-1')
+    option_10k = slide_bar.elements(:class, 'rc-slider-mark-text')[2]
+    left_handle.drag_and_drop_on(option_10k)
+  end
+
+  def random_college
+    # pick a random college, go to showpage
+    college = colleges.sample
+    college_name = college.element(:tag_name, 'h4').text
+    college.click; sleep 1
+
+    college_name
+  end
+
+  def check_enrollment_size
+    failure = []
+    colleges.each do |c|
+      school = c.element(:tag_name, 'h4').text
+      subtitle = c.element(:class, 'card-details').element(:class, 'subtitle').text
+      enrollments = subtitle.split(' ').last.gsub(',', '').to_i
+      msg = "#{school} has #{enrollments} enrollments, expected >= 10k"
+      failure << msg unless (enrollments >= 10000)
+    end
+    assert_empty failure
+  end
+
+  def check_in_state_tuition(college_name)
+    # extract in state tuition from profile and compare
+    profile = @browser.element(:class, 'college-profile')
+    col = profile.div(:class, 'col-lg-7')
+    table = col.tables(:class, 'box')[1]
+    tuition = table.trs[1].td(:index, 0).text.gsub(/[$,]/, '').to_i
+    assert tuition >= 10000, "#{college_name} tuition is #{tuition}, expected >= 10k"
+  end
+
+  def test_filter_by_enrollment_size
+    open_filter
+    select_filter('ranger-slider-enrollmentRange')
+    apply_filters
+    check_enrollment_size
+  end
+
+  def test_filter_by_tuition
+    open_filter
+    select_filter('ranger-slider-tuitionRange')
+    apply_filters
+    check_in_state_tuition(random_college)
+  end
+
+  def test_filter_by_enrollment_tuition
+    open_filter
+    select_filter('ranger-slider-enrollmentRange')
+    select_filter('ranger-slider-tuitionRange')
+    apply_filters
+    check_enrollment_size
+    check_in_state_tuition(random_college)
+  end
+end
