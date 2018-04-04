d8d8a2ea4830171b59bca4921cbd8e3e6ad58547
Tiffany Rea <tiffy.nets@gmail.com>
2018-04-04 10:35:34 -0500

    TS-394 Daily Monitor - Domain Versions/Redirects - automation script created

---
+++ test/daily_monitor/domain_versions_redirect_test.rb
@@ -0,0 +1,57 @@
+# encoding: utf-8
+require_relative '../test_helper'
+
+# Daily Mornitor: TS-394
+# UI Test: Daily Monitor - Domain Versions/Redirects
+class DomainVersionsRedirectTest < Minitest::Test
+  def setup
+    @expect_200 = [
+      'http://www.ncsasports.org',
+      'https://www.ncsasports.org/blog/',
+      'http://www.reigningchamps.com',
+      'ec2-54-84-46-169.compute-1.amazonaws.com',
+      'ec2-52-23-217-154.compute-1.amazonaws.com',
+      'http://ec2-52-23-177-122.compute-1.amazonaws.com/#!/',
+      'http://reigningchamps.com/',
+      'http://www.athleticscholarships.com',
+      'http://www.athleteswanted.com',
+      'https://getrecruited.ncsasports.org',
+      'http://verifiedid.ncsasports.org',
+      'http://allinaward.com/'
+    ]
+
+    @expect_301 = [
+      'https://www.ncsasports.org',
+      'http://ncsasports.org',
+      'https://ncsasports.org',
+      'http://www.ncsasports.com',
+      'http://www.ncsasports.org/blog',
+      'http://ncsasports.org/blog',
+      'https://www.ncsasports.org/blog',
+      'https://ncsasports.org/blog',
+      'https://ncsasports.org/blog/'
+    ]
+  end
+
+  def test_domain_redirect
+    no_redir = []; bad_resp = []
+    @expect_301.each do |url|
+      resp = RestClient::Request.execute(method: :get, url: url)
+      bad_resp << "#{resp.code} - #{url}" unless resp.code.eql? 200
+      no_redir << "#{url}" unless resp.history.last.code.eql? 301
+    end
+    assert_empty no_redir
+    assert_empty bad_resp
+  end
+
+  def test_domain_no_redirect
+    bad_resp = []; redir = []
+    @expect_200.each do |url|
+      resp = RestClient::Request.execute(method: :get, url: url)
+      bad_resp << "#{resp.code} - #{url}" unless resp.code.eql? 200
+      redir << "#{url} - #{resp.history}" unless resp.history.empty?
+    end
+    assert_empty bad_resp
+    assert_empty redir
+  end
+end
