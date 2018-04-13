62aaf753577c908d27b2cf4e9cdcf86b39752ed3
Tiffany Rea <tiffy.nets@gmail.com>
2018-04-13 10:59:54 -0500

    trying different synctax for shell script

---
+++ grid_check.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Check and wait for Selenium Grid connection
+# Use http://localhost:4444/wd/hub/status locally
+
+echo 'Checking Selenium Grid ready status'
+condition='false'
+while [ '$condition' -eq 'false' ]; do
+  curl http://172.17.0.2:24444/wd/hub/status > json
+  cat json
+  condition=$(jq '.value.ready' json)
+done
+
+rm json
