154067d802dc9ed425d17bf701e3ce346ad460c6
Tiffany Rea <tiffy.nets@gmail.com>
2018-04-13 11:32:03 -0500

    fix grid health check syntax

--- script/grid_check.sh
+++ script/grid_check.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # Check and wait for Selenium Grid connection
-# Use http://localhost:4444/wd/hub/status locally
 
 echo 'Checking Selenium Grid ready status'
 condition=false
 while [ !${condition} ]; do
-  curl http://172.17.0.2:24444/wd/hub/status > json
+  curl http://localhost:4444/wd/hub/status > json
   cat json
   condition=$(jq '.value.ready' json)
 done
