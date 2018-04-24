d2b5df25e587ca733828cd92674c8b1c39eb5681
Tiffany Rea <tiffy.nets@gmail.com>
2018-04-24 17:13:32 -0500

    sigh .. fixing typo

--- test/ted/athlete_evaluation_test.rb
+++ test/ted/athlete_evaluation_test.rb
@@ -46,7 +46,7 @@ class AthleteEvaluationTest < Common
   def reset_rating
     rating_area = @browser.element(:class, 'star-rating').element(:class, 'rating')
     rating_area.element(:for, 'star1').click
-    Watir::Wait.until { TED.modal.preset? }
+    Watir::Wait.until { TED.modal.present? }
     TED.modal.button(:text, 'Confirm').click; sleep 1
   end
 
