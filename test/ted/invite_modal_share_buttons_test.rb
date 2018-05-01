4997b7f5166aafb0cc0bcdadbf1207649691edb4
Eli Smukler <eli@berecruited.com>
2018-05-01 14:04:57 -0700

    Fix refactor; add failure messages

--- test/ted/invite_modal_share_buttons_test.rb
+++ test/ted/invite_modal_share_buttons_test.rb
@@ -16,37 +16,44 @@ class InviteModalShareButtonsTest < Common
 
   def check_if_invite_modal_opens
     @browser.button(:class, 'add-btn').click
-    sleep 1
-    assert 'Invite Athletes', @browser.element(:id, 'myModalLabel').text
+    UIActions.wait_for_modal
+    modal_title = 'Invite Athletes'
+
+    assert_equal modal_title,
+      TED.modal.element(:id, 'myModalLabel').text,
+      "Expected #{modal_title} modal to open"
   end
 
   def check_copy_button
     copy_input = @browser.element(:class, 'input-group').element(:tag_name, 'input')
 
-    assert_equal
-      "https://team-staging.ncsasports.org/teams/awesome-sauce/sign_up",
-      copy_input.attribute('value')
+    assert_equal 'https://team-staging.ncsasports.org/teams/awesome-sauce/sign_up',
+      copy_input.attribute('value'),
+      'Incorrect Sign Up page URL in Copy link'
   end
 
   def check_mail_button
     mail_to_link = @browser.element(:class, 'share-buttons').element(:class, 'fa-envelope').parent
-    assert_match /^mailto:\?subject=Start%20your%20Awesome%20Sauce/, mail_to_link.attribute('href')
+
+    assert_match /^mailto:\?subject=Start%20your%20Awesome%20Sauce/,
+      mail_to_link.attribute('href'),
+      'Incorrect URL for mailto link'
   end
 
   def check_fb_button
     fb_link = @browser.element(:class, 'share-buttons').elements(:tag_name, 'a')[1]
 
-    assert_match
-      /^https:\/\/www\.facebook\.com\/sharer\/sharer\.php\?u=https:\/\/team-staging\.ncsasports\.org\/teams\/awesome-sauce\/sign_up&quote=You/,
-      fb_link.attribute('href')
+    assert_match /^https:\/\/www\.facebook\.com\/sharer\/sharer\.php\?u=https:\/\/team-staging\.ncsasports\.org\/teams\/awesome-sauce\/sign_up&quote=You/,
+      fb_link.attribute('href'),
+      'Incorrect URL for Facebook button'
   end
 
   def check_twitter_button
     twitter_link = @browser.element(:class, 'share-buttons').elements(:tag_name, 'a')[2]
 
-    assert_match
-      /https:\/\/twitter\.com\/intent\/tweet\?url=https:\/\/team-staging\.ncsasports\.org\/teams\/awesome-sauce\/sign_up&text=You/,
-      twitter_link.attribute('href')
+    assert_match /https:\/\/twitter\.com\/intent\/tweet\?url=https:\/\/team-staging\.ncsasports\.org\/teams\/awesome-sauce\/sign_up&text=You/,
+      twitter_link.attribute('href'),
+      'Incorrect URL for Twitter button'
   end
 
   def check_go_to_button
@@ -54,9 +61,9 @@ class InviteModalShareButtonsTest < Common
     go_to_link.click
     @browser.windows.last.use
 
-    assert_equal
-      @browser.element(:tag_name, 'h2').text,
-      'AWESOME SAUCE TEAMS UP WITH NCSA TO GIVE YOU A RECRUITING ADVANTAGE'
+    assert_equal @browser.element(:tag_name, 'h2').text,
+      'AWESOME SAUCE TEAMS UP WITH NCSA TO GIVE YOU A RECRUITING ADVANTAGE',
+      'Incorrect title on Sign Up page after clicking go-to link'
   end
 
   def test_invite_modal_share_buttons
