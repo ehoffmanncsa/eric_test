17f50c7d776f947ae6957bbe23b5476c19866c31
Eli Smukler <eli@berecruited.com>
2018-05-08 15:59:28 -0700

    finish checking for restored athlete

--- test/ted/restored_athlete_request_test.rb
+++ test/ted/restored_athlete_request_test.rb
@@ -6,42 +6,12 @@ require_relative '../test_helper'
 class RestoredAthleteRequestTest < Common
   def setup
     super
-    # POSSetup.setup(@browser)
     TED.setup(@browser)
   end
 
   def teardown
     super
   end
-
-  def open_add_athlete_modal
-    @browser.button(:text, 'Invite Athletes').click
-    TED.modal.button(:text, 'Manually Add Athlete').click
-    TED.modal.button(:text, 'Add Athlete').click; sleep 1
-  end
-  #
-  #
-  def send_invite_email
-    # find and click the not sent button for the newly added athlete
-    # make sure Edit Athlete modal shows up before proceeding
-    row = @browser.element(:text, athlete_email).parent
-    row.elements(:tag_name, 'td')[4].element(:class, 'btn-primary').click; sleep 1
-    assert TED.modal.visible?, 'Edit Athlete modal not found'
-
-    TED.modal.button(:text, 'Save & Invite').click
-    UIActions.wait_for_modal
-
-    # refresh the page and go back to athlete tab
-    # make sure athlete status is now pending after email sent
-    status = row.elements(:tag_name, 'td')[4].text
-    assert_equal status, 'Pending', "Expected status #{status} to be Pending"
-    TED.sign_out
-  end
-
-  def login_as_athlete_in_clientrms
-    UIActions.user_login(athlete_email)
-  end
-
 
   def store_accepted_athlete_info
     TEDAthleteApi.setup
@@ -50,16 +20,13 @@ class RestoredAthleteRequestTest < Common
     refute @athlete['id'].nil?, 'No accepted athletes found'
   end
 
-  def deactivate_athlete
+  def delete_athlete
     deleted_athlete = TEDAthleteApi.delete_athlete(@athlete['id'])
     assert deleted_athlete['attributes']['deleted'], 'Athlete not deleted successfully'
   end
 
-  def athlete_email
-    @athlete['attributes']['profile']['email']
-  end
-
   def add_athlete
+    UIActions.ted_login
     TED.go_to_athlete_tab
     open_add_athlete_modal
 
@@ -79,14 +46,57 @@ class RestoredAthleteRequestTest < Common
     assert (@browser.element(:text, athlete_email).present?), "Cannot find newly added Athlete #{athlete_email}"
   end
 
+  def open_add_athlete_modal
+    @browser.button(:text, 'Invite Athletes').click
+    TED.modal.button(:text, 'Manually Add Athlete').click
+    TED.modal.button(:text, 'Add Athlete').click; sleep 1
+  end
+
+  def athlete_email
+    @athlete['attributes']['profile']['email']
+  end
+
+  def send_invite_email
+    # find and click the not sent button for the newly added athlete
+    # make sure Edit Athlete modal shows up before proceeding
+    row = @browser.element(:text, athlete_email).parent
+    row.elements(:tag_name, 'td')[4].element(:class, 'btn-primary').click; sleep 1
+    assert TED.modal.visible?, 'Edit Athlete modal not found'
+
+    TED.modal.button(:text, 'Save & Invite').click
+    UIActions.wait_for_modal
+
+    # refresh the page and go back to athlete tab
+    # make sure athlete status is now pending after email sent
+    status = row.elements(:tag_name, 'td')[4].text
+    assert_equal status, 'Pending', "Expected status #{status} to be Pending"
+    TED.sign_out
+  end
+
+  def check_for_athlete_pop_up
+    UIActions.user_login(athlete_email)
+
+    assert @browser.element(:class, 'club-popup-js').present?, 'TED Invite Request Modal not appearing.'
+  end
+
+  def grant_access
+    @browser.element(:id, 'club-yes').click
+  end
+
+  def check_athlete_is_accepted
+    accepted_athletes = TEDAthleteApi.find_athletes_by_status('accepted')
+    accepted_athlete_ids = accepted_athletes.map { |ath| ath['attributes']['profile']['email'] }
+    assert_includes accepted_athlete_ids, athlete_email, "#{athlete_email} not found in list of accepted athletes"
+  end
+
   def test_restore_athlete_with_request
     store_accepted_athlete_info
-    deactivate_athlete
-    UIActions.ted_login
+    delete_athlete
+
     add_athlete
     send_invite_email
-    login_as_athlete_in_clientrms
-    # grant_access
-    # check_athlete_is_accepted
+    check_for_athlete_pop_up
+    grant_access
+    check_athlete_is_accepted
   end
 end
