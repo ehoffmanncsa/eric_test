7dffe6b6a52ad9ce107a2aea195f600577b84df1
Eric <ehoffman@ncsasports.org>
2019-11-25 11:06:44 -0600

    Athlete export (#100)
    * Tracked Athletes export csv
    
    * athlete export csv

---
+++ test/coachlive-fe/athlete_export.rb
@@ -0,0 +1,82 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+
+# This test will export tracked athletes in a csv to the
+#  ncsa.automation@gmail.com CoachLive inbox.
+#  We are unable to open the csv in the email at this time
+
+class CoachLiveExportTest < Common
+  def setup
+    super
+    @gmail = GmailCalls.new
+    @gmail.get_connection
+
+    AthleticEventUI.setup(@browser)
+  end
+
+  def teardown
+    super
+  end
+
+  def click_menu
+    @browser.element('data-icon': 'bars').click
+    sleep 3
+  end
+
+  def open_athlete
+    @browser.element(xpath: "//a[@href='/athletes/tracked']").click
+    sleep 5
+  end
+
+  def select_athletes
+    i = 0
+    rand(2 .. 7).times do |i|
+      checkbox = @browser.elements('data-icon': 'square').to_a
+      checkbox.sample.click
+      sleep 2
+      i += 1
+    end
+  end
+
+  def click_ellipsis
+    @browser.element('data-icon': 'ellipsis-h').click
+    sleep 3
+  end
+
+  def select_export
+    @browser.element(role: 'menuitem').click
+    sleep 3
+  end
+
+  def click_ok
+    ok = @browser.button(text: 'OK')
+    ok.click
+  end
+
+  def close_app
+    @browser.button(text: 'Log out').click
+  end
+
+  def log_into_Coach_Packet
+    AthleticEventUI.adjust_window
+    AthleticEventUI.request_login
+    login_url = AthleticEventUI.get_login_url
+    @browser.goto login_url
+    sleep 1
+    AthleticEventUI.delete_email
+  end
+
+  def test_athlete_export
+    log_into_Coach_Packet
+    click_menu
+    open_athlete
+    select_athletes
+    click_ellipsis
+    select_export
+    AthleticEventUI.get_login_url
+    AthleticEventUI.delete_email
+    click_menu
+    close_app
+  end
+end
