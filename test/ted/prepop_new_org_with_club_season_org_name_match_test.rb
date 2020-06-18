2f0791226611dae6c029a1631de0b3a8c404256d
Eric Hoffman <ehoffman@KB-MB-033.ncsasports.org>
2020-06-18 09:12:51 -0500

    added skip for ted prepop scripts

--- test/ted/prepop_new_org_with_club_season_org_name_match_test.rb
+++ test/ted/prepop_new_org_with_club_season_org_name_match_test.rb
@@ -15,6 +15,7 @@ require_relative '../test_helper'
 class CreateNewOrgWithPrepop < Common
   def setup
     super
+    skip
     C3PO.setup(@browser)
     MSSetup.setup(@browser)
     TED.setup(@browser)
