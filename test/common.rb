8d8d6b8d012342dd8d270707749cae9947ee9d88
Randy Wilson <rwilson@ncsasports.org>
2021-05-24 14:03:04 -0500

    Testing firefox 5.

--- test/common.rb
+++ test/common.rb
@@ -1,7 +1,9 @@
 # encoding: utf-8
 class Common < Minitest::Test
  def setup
+   Selenium::WebDriver::Firefox::Binary.path="/usr/lib/firefox"
    @ui = UI.new 'docker', 'firefox'
+   # @ui = UI.new 'local', 'firefox'
    @browser = @ui.driver
    UIActions.setup(@browser)
  end
